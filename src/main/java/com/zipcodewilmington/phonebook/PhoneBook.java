b4229046c55d272b5575144fd1d0884ad1017054
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-11-21 19:48:05 -0500

    compiles

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,10 +1,7 @@
 package com.zipcodewilmington.phonebook;
 
 import java.util.List;
-import java.util.ArrayList;
 //import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
@@ -32,7 +29,7 @@ public class PhoneBook {
     public void remove(String name) {
     }
 
-    public Boolean hasEntry(String name) {
+    public Boolean hasEntry(String name, String phoneNumber) {
         return null;
     }
 
